a7eafa36da5f3315290addeb382e4f68e632cd08
Gina Häußge <gina@octoprint.org>
2022-06-13 11:24:30 +0200

    🔖 Preparing release of 2022.6.13

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ plugin_package = "octoprint_pi_support"
 plugin_name = "OctoPrint-PiSupport"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "2022.3.28"
+plugin_version = "2022.6.13"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
